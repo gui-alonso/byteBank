38375d02dfa9e9d550d79168a33d612f312b7862
Guilherme Alonso <alonsogui011@gmail.com>
2023-02-20 14:21:22 -0300

    projeto byteBank

---
+++ ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace byteBank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace byteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# com Orientação a Objetos");
+            Console.WriteLine("---------------------------");
+
+
+
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("PRESSIONE ENTER PARA SAIR!");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
